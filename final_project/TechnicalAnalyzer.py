916f29e7e5cf12fd9f2babe07e696102acd28ce9
oded-hodsh <oded1258@gmail.com>
2022-02-06 22:37:40 +0200

    TechnicalAnalyzer: add close price noramlize function.

--- final_project/TechnicalAnalyzer.py
+++ final_project/TechnicalAnalyzer.py
@@ -1,7 +1,21 @@
+import pandas as pd
 
 
 class TechnicalAnalyzer:
+    @staticmethod
+    def add_technical_features(stock_data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Add technical features to the stock data Dataframe.
+        :param stock_data: Stock data we get from StockDataFetcher module.
+        """
+        pass
 
-    def __init__(self):
-        return 0
-
+    @staticmethod
+    def get_normalized_price(stock_data: pd.DataFrame) -> pd.Series:
+        """
+        Normalize the stock close data to be in range of 0 and 1.
+        """
+        close_data_price = stock_data['Close']
+        min_close_price_value = min(close_data_price)
+        close_data_price_range = max(close_data_price) - min_close_price_value
+        return (close_data_price - min_close_price_value) / close_data_price_range
