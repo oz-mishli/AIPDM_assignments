80333828704b64baf3e00f869b8d1c899ff49977
oded-hodsh <oded1258@gmail.com>
2022-02-07 22:01:33 +0200

    TechnicalAnalyzer: added returns by span and period as in paper's formula

--- final_project/TechnicalAnalyzer.py
+++ final_project/TechnicalAnalyzer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -19,3 +20,21 @@ class TechnicalAnalyzer:
         min_close_price_value = min(close_data_price)
         close_data_price_range = max(close_data_price) - min_close_price_value
         return (close_data_price - min_close_price_value) / close_data_price_range
+
+    @staticmethod
+    def get_return(stock_data: pd.DataFrame, current_index: int, past_index: int, variance_span: int,
+                   exp_weight: float) -> float:
+        """
+        Calculate return relevant to past price.
+        """
+        if current_index < past_index:
+            raise ValueError('Past index should be larger than current_index.')
+
+        close_data = stock_data['Close']
+        r_t = close_data[current_index] - close_data[past_index]
+        daily_r_t = pd.Series(np.array(close_data[current_index - variance_span:current_index]) - \
+                              np.array(close_data[current_index - variance_span - 1:current_index - 1]))
+        weights = np.array([max(exp_weight ** power, 0.0001) for power in range(variance_span - 1, -1, -1)]) * (
+                1 - exp_weight)
+        sigma_t = (daily_r_t * weights).var()
+        return r_t / (sigma_t * np.sqrt(current_index - past_index))
